61a7de810d3e758d8e682709c95afa3abe2fbdf4
keren <profmaryagoyi@gmail.com>
2022-01-09 14:51:13 +0200

    make it better

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 import express from "express"
 import routes from  "./routes/routes.js"
 import bodyParser from "body-parser"
+import cors from "cors";
 const app= express();
-const PORT = process.env.PORT || 5500;
-
+const tasks= {
+  id: 1, name:"idAddress",message:"hello"
+}
+const PORT = process.env.PORT || 5000;
+app.use(cors())
 app.use(bodyParser.json());
+app.set("view engine", 'ejs');
+app.use(express.static('public'));
+//app.use(bodyParser.urlencoded);
 app.get("/", (req, res) =>{
-   const idAddress = req.header('x-forwarded-for') || req.connection.remoteAddress;
+const ipAddress = req.header('x-forwarded-for') || req.connection.remoteAddress;
+const browserType = req.header("Sec-Ch-Ua");
+const os =req.header("Sec-Ch-Ua-Platform");
+const  browser =req.headers['content-type'];
+const viewp =req.header["Viewport-Width"];
+const isIpad = !!req.headers['user-agent'].match(/Mozilla/);
 
-    res.json({"Your IPadrress": idAddress });
+
+  
+    res.render("app", {ip: ipAddress, bt: browserType,os,browser, viewp});
 })
 
 app.use("/api/books", routes);
 
-//app.use(bodyParser.urlencoded);
+//
 
 app.listen(PORT, () =>{
 
