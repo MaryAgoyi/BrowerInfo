5d772b5a006b98bb4860f04311a7cf243101ee8f
profmary <83574522+profmary@users.noreply.github.com>
2022-01-11 22:23:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,31 +3,20 @@ import routes from  "./routes/routes.js"
 import bodyParser from "body-parser"
 import cors from "cors";
 const app= express();
-const tasks= {
-  id: 1, name:"idAddress",message:"hello"
-}
+
 const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(bodyParser.json());
 app.set("view engine", 'ejs');
 app.use(express.static('public'));
-//app.use(bodyParser.urlencoded);
+
 app.get("/", (req, res) =>{
 const ipAddress = req.header('x-forwarded-for') || req.connection.remoteAddress;
-const browserType = req.header("Sec-Ch-Ua");
-const os =req.header("Sec-Ch-Ua-Platform");
-const  browser =req.headers['content-type'];
-const viewp =req.header["Viewport-Width"];
-const isIpad = !!req.headers['user-agent'].match(/Mozilla/);
 
-
-  
-    res.render("app", {ip: ipAddress, bt: browserType,os,browser, viewp});
+  res.render("app", {ip: ipAddress});
 })
 
-app.use("/api/books", routes);
 
-//
 
 app.listen(PORT, () =>{
 
